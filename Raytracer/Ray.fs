bb1b6231387ecd0aca96d89692b74c1c548ca880
rrokkam <rohithrokkam@gmail.com>
2023-02-19 14:51:45 -0800

    red sphere in foreground

--- Raytracer/Ray.fs
+++ Raytracer/Ray.fs
@@ -6,7 +6,20 @@ type ray = { origin: point3; direction: vec3 }
 
 let at r (t: float) : point3 = r.origin + t * r.direction
 
+let hit_sphere center radius r =
+    let oc = r.origin - center
+    let a = r.direction * r.direction
+    let b = 2.0 * oc * r.direction
+    let c = oc * oc - radius * radius
+    let discriminant = b * b - 4.0 * a * c
+    discriminant > 0
+
 let ray_color r =
-    let direction = normalize r.direction
-    let t = 0.5 * (direction.e1 + 1.0)
-    (1.0 - t) * { r = 1.0; g = 1.0; b = 1.0 } + t * { r = 0.5; g = 0.7; b = 1.0 }
+    let sphere_center = { point3.zero with e2 = -1.0 }
+
+    if hit_sphere sphere_center 0.5 r then
+        { r = 1; g = 0; b = 0 }
+    else
+        let direction = normalize r.direction
+        let t = 0.5 * (direction.e1 + 1.0)
+        (1.0 - t) * { r = 1.0; g = 1.0; b = 1.0 } + t * { r = 0.5; g = 0.7; b = 1.0 }
--- Raytracer/Vector.fs
+++ Raytracer/Vector.fs
@@ -21,10 +21,7 @@ type vec3 =
     static member (~-)(v: vec3) = -1.0 * v
     static member (-)(u: vec3, v: vec3) = u + -v
 
-    static member (*)(u, v) =
-        { e0 = u.e0 * v.e0
-          e1 = u.e1 * v.e1
-          e2 = u.e2 * v.e2 }
+    static member (*)(u, v) = u.e0 * v.e0 + u.e1 * v.e1 + u.e2 * v.e2
 
     static member (^)(u, v) =
         { e0 = u.e1 * v.e2 - u.e2 * v.e1
