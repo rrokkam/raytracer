26e7e937e51116891358c9cc10be5a12df14879c
rrokkam <rohithrokkam@gmail.com>
2023-02-25 15:05:56 -0800

    try getting rid of option

--- Raytracer/Sphere.fs
+++ Raytracer/Sphere.fs
@@ -9,7 +9,16 @@ type sphere =
       radius: float
       material: material }
 
-let test r t_min t_max s =
+let hit_record (r: ray) s t =
+    let n = (r.at t - s.center) / s.radius
+    let front_face = r.direction * n < 0
+
+    { N = if front_face then n else -n
+      p = r.at t
+      t = t
+      front_face = front_face }
+
+let test r s =
     let oc = r.origin - s.center
     let a = len_squared r.direction
     let half_b = oc * r.direction
@@ -17,35 +26,21 @@ let test r t_min t_max s =
     let fourth_discriminant = half_b ** 2 - a * c
 
     if fourth_discriminant < 0 then
-        None
+        []
     else
         // the value of t where the ray intersects the sphere
         let t1 = (-half_b - sqrt fourth_discriminant) / a
         let t2 = (-half_b + sqrt fourth_discriminant) / a
 
-        let hit_record t =
-            let n = (r.at t - s.center) / s.radius
-            let front_face = r.direction * n < 0
-            let sign = if front_face then 1.0 else -1.0
-
-            { N = sign * n
-              p = r.at t
-              t = t
-              front_face = front_face }
-
-        if t1 > t_min && t1 < t_max then
-            Some(t1, hit_record t1, s.material)
-        else if t2 > t_min && t2 < t_max then
-            Some(t2, hit_record t2, s.material)
-        else
-            None
+        [ (t1, hit_record r s t1, s.material); (t2, hit_record r s t2, s.material) ]
 
 let minBy f l =
     if List.isEmpty l then None else Some(l |> List.minBy f)
 
 let test_many r (spheres: sphere list) t_min t_max : (intersect * material) option =
     spheres
-    |> List.map (test r t_min t_max)
-    |> List.choose id
+    |> List.map (test r)
+    |> List.collect id
+    |> List.filter (fun (t, _, _) -> t > t_min && t < t_max)
     |> minBy (fun (t, _, _) -> t)
     |> Option.map (fun (_, ix, mat) -> (ix, mat))
